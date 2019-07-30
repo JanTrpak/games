50c3746a56fea5e4395c6a7036f77b832b9d3ec1
Jan Trpák <jtkrabz@gmail.com>
2019-07-30 18:12:02 +0200

    T1 - expand logic, return IGame

--- src/modules/game/game.service.ts
+++ src/modules/game/game.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import Axios, { AxiosResponse } from 'axios';
-import { ICheapSharkResponse } from './interfaces/cheapSharkResponse.interface';
+import { ICheapSharkDealsByIdResponse } from './interfaces/cheapSharkDealsByIdResponse.interface';
+import { IGame } from './interfaces/game.interface';
 
 @Injectable()
 export class GameService {
@@ -8,15 +9,43 @@ export class GameService {
   /**
    * Returns list of games
    */
-  public async getGames(): Promise<ICheapSharkResponse[]> {
+  public async getGames(): Promise<IGame[]> {
     const gamesList = await this.fetchGameInfo();
-    return gamesList.data;
+
+    const games: IGame[] = gamesList.data.map(
+      async (inputDeal: { dealID: string; }) => {
+        const decodedDealId = decodeURIComponent(inputDeal.dealID);
+        const dealObject = await this.fetchDealsById(decodedDealId);
+        
+        return {
+          name: dealObject.gameInfo.name,
+          salePrice: Number(dealObject.gameInfo.salePrice),
+          cheapestPrice: Number(dealObject.cheapestPrice.price),
+          releaseDate: new Date(dealObject.gameInfo.releaseDate * 1000),
+        };
+      });
+    const output = await Promise.all(games);
+
+    return output;
   }
 
   private async fetchGameInfo(): Promise<AxiosResponse> {
     const res = await Axios.get('http://www.cheapshark.com/api/1.0/deals?storeID=1&desc=0&title=grand%20theft%20auto&pageSize=20');
     if (res.status === 200) {
       return res;
+    }
+    throw new Error('Error fetching games data');
+  }
+
+  private async fetchDealsById(id: string): Promise<ICheapSharkDealsByIdResponse> {
+    const res = await Axios.get('http://www.cheapshark.com/api/1.0/deals', {
+      params: {
+        id,
+      },
+    });
+
+    if (res.status === 200) {
+      return res.data;
     }
     throw new Error('Error fetching games data');
   }
